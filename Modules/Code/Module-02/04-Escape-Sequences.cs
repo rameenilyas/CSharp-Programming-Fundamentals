30993117bc26468d810eae9e013e7790f3e6f93d
rameenilyas <rameenilyaskhan2006@gmail.com>
2026-08-07 09:09:30 -0700

    Create 04-Escape-Sequences.cs

---
+++ Modules/Code/Module-02/04-Escape-Sequences.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello\nWorld");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Name\tAge");
+        Console.WriteLine("Ali\t20");
+    }
+}
