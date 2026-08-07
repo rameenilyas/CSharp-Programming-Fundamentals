a19ef5bf487c8d7833cb87ec157dbb9bd38f2525
rameenilyas <rameenilyaskhan2006@gmail.com>
2026-08-07 09:00:12 -0700

    Create 02-Comments.cs

---
+++ Modules/Code/Module-02/02-Comments.cs
@@ -0,0 +1,16 @@
+using System;
+
+// Single-line comment example
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+         This is a
+         multi-line comment.
+        */
+
+        Console.WriteLine("Comments in C#");
+    }
+}
