ce2542c5d35bdecabf8cbfb71d80b24812f7f012
rameenilyas <rameenilyaskhan2006@gmail.com>
2026-08-08 12:06:10 -0700

    Update VariablesAndDataTypes.cs

--- Modules/Code/Module-03/VariablesAndDataTypes.cs
+++ Modules/Code/Module-03/VariablesAndDataTypes.cs
@@ -1,1 +1,37 @@
 
+using System;
+
+class VariablesAndDataTypes
+{
+    static void Main()
+    {
+        // Variable
+        int age = 20;
+
+        // Constant
+        const double PI = 3.14;
+
+        // Float
+        float temperature = 36.5f;
+
+        // Double
+        double percentage = 87.56;
+
+        // Char
+        char grade = 'A';
+
+        // Boolean
+        bool isStudent = true;
+
+        // String
+        string name = "Ali";
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("PI: " + PI);
+        Console.WriteLine("Temperature: " + temperature);
+        Console.WriteLine("Percentage: " + percentage);
+        Console.WriteLine("Grade: " + grade);
+        Console.WriteLine("Is Student: " + isStudent);
+    }
+}
