008fff353ae5dc3f11f22f9920bb5b392bb52b4a
rameenilyas <rameenilyaskhan2006@gmail.com>
2026-08-08 12:22:38 -0700

    Update StudentProfile.cs

--- Projects/Module-03-Student-Profile-System/StudentProfile.cs
+++ Projects/Module-03-Student-Profile-System/StudentProfile.cs
@@ -1,1 +1,31 @@
 
+using System;
+
+class StudentProfile
+{
+    static void Main()
+    {
+        // Student Information
+        string name = "Ali";
+        int age = 20;
+        string city = "Karachi";
+        float marks = 85.5f;
+        char grade = 'A';
+        bool isPassed = true;
+
+        // Constant
+        const string university = "SMIU";
+
+        // Display Student Profile
+        Console.WriteLine("===== Student Profile =====");
+        Console.WriteLine();
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("City: " + city);
+        Console.WriteLine("Marks: " + marks);
+        Console.WriteLine("Grade: " + grade);
+        Console.WriteLine("Passed: " + isPassed);
+        Console.WriteLine("University: " + university);
+    }
+}
