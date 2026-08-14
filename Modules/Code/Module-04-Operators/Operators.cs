b3be4e89ee2347f1c995fc8fa4fbdbcc532bef45
rameenilyas <rameenilyaskhan2006@gmail.com>
2026-08-14 11:21:50 -0700

    Update Operators.cs

--- Modules/Code/Module-04-Operators/Operators.cs
+++ Modules/Code/Module-04-Operators/Operators.cs
@@ -1,1 +1,19 @@
+using System;
 
+class Operators
+{
+    static void Main()
+    {
+        // Arithmetic Operators
+        int a = 10;
+        int b = 3;
+
+        Console.WriteLine("Arithmetic Operators");
+
+        Console.WriteLine("Addition: " + (a + b));
+        Console.WriteLine("Subtraction: " + (a - b));
+        Console.WriteLine("Multiplication: " + (a * b));
+        Console.WriteLine("Division: " + (a / b));
+        Console.WriteLine("Remainder: " + (a % b));
+    }
+}
