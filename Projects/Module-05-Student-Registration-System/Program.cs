1c922ace418ecfdd385f32c47721f76c541eee08
rameenilyas <rameenilyaskhan2006@gmail.com>
2026-08-15 09:05:11 -0700

    Update Program.cs

--- Projects/Module-05-Student-Registration-System/Program.cs
+++ Projects/Module-05-Student-Registration-System/Program.cs
@@ -49,3 +49,46 @@ Registration Completed!
 ```
 
 
+//code
+using System;
+
+class StudentRegistrationSystem
+{
+    static void Main()
+    {
+        // Student Information Input
+
+        Console.Write("Enter Student Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Roll Number: ");
+        int rollNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter City: ");
+        string city = Console.ReadLine();
+
+        Console.Write("Enter Marks: ");
+        int marks = Convert.ToInt32(Console.ReadLine());
+
+
+        // Formatted Output
+
+        Console.WriteLine();
+        Console.WriteLine("================================");
+        Console.WriteLine("       STUDENT PROFILE");
+        Console.WriteLine("================================");
+
+        Console.WriteLine($"Name        : {name}");
+        Console.WriteLine($"Age         : {age}");
+        Console.WriteLine($"Roll Number : {rollNumber}");
+        Console.WriteLine($"City        : {city}");
+        Console.WriteLine($"Marks       : {marks}");
+
+        Console.WriteLine("================================");
+        Console.WriteLine("Registration Completed!");
+        Console.WriteLine("================================");
+    }
+}
