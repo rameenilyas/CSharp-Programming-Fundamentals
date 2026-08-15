6c0c37af86ec8405e6d8594a5c2dc3994a6bbcb8
rameenilyas <rameenilyaskhan2006@gmail.com>
2026-08-15 08:51:51 -0700

    Update Program.cs

--- Modules/Code/Module-05/UserInputOutput/Program.cs
+++ Modules/Code/Module-05/UserInputOutput/Program.cs
@@ -1,1 +1,40 @@
+using System;
 
+class UserInputOutput
+{
+    static void Main()
+    {
+        // 1. User Input
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+
+        // 2. Output
+        Console.WriteLine();
+        Console.WriteLine("Your name is: " + name);
+        Console.WriteLine("Your age is: " + age);
+
+
+        // 3. Multiple Inputs
+        Console.WriteLine();
+
+        Console.Write("Enter your roll number: ");
+        int rollNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter your marks: ");
+        int marks = Convert.ToInt32(Console.ReadLine());
+
+
+        // 4. Formatting Output
+        Console.WriteLine();
+        Console.WriteLine("--- Student Information ---");
+
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"Roll Number: {rollNumber}");
+        Console.WriteLine($"Marks: {marks}");
+    }
+}
