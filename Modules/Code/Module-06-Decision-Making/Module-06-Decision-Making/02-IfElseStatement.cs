bbaa0627dfa89bc422fffde576630dc02519b1c2
rameenilyas <rameenilyaskhan2006@gmail.com>
2026-08-17 09:53:06 -0700

    Create 02-IfElseStatement.cs

---
+++ Modules/Code/Module-06-Decision-Making/Module-06-Decision-Making/02-IfElseStatement.cs
@@ -0,0 +1,18 @@
+using System;
+
+class IfElseStatement
+{
+    static void Main()
+    {
+        int marks = 40;
+
+        if (marks >= 50)
+        {
+            Console.WriteLine("Pass");
+        }
+        else
+        {
+            Console.WriteLine("Fail");
+        }
+    }
+}
