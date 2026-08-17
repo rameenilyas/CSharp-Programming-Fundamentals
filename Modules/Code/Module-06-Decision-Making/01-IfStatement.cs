2b937c2122fd29a5df94b0c5e80a51b7adf0a9bc
rameenilyas <rameenilyaskhan2006@gmail.com>
2026-08-17 09:52:25 -0700

    Create 01-IfStatement.cs

---
+++ Modules/Code/Module-06-Decision-Making/01-IfStatement.cs
@@ -0,0 +1,14 @@
+using System;
+
+class IfStatement
+{
+    static void Main()
+    {
+        int age = 20;
+
+        if (age >= 18)
+        {
+            Console.WriteLine("You are an adult.");
+        }
+    }
+}
