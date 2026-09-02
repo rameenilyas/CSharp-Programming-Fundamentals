16eb5c65d84c7e458e5c202dd58f8438b2ea70e0
rameenilyas <rameenilyaskhan2006@gmail.com>
2026-09-02 09:29:36 -0700

    Update SquarePattern.cs

--- Modules/Code/Code-8/SquarePattern.cs
+++ Modules/Code/Code-8/SquarePattern.cs
@@ -1,1 +1,17 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 4; i++)
+        {
+            for (int j = 1; j <= 4; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
